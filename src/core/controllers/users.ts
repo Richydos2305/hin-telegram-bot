b03256e9fda9916e18c7d71a65b4b835a87f8c7e
Richydos2305 <richardosunmu@gmail.com>
2024-12-22 17:36:58 +0100

    Setup User Model (#3)

--- src/core/controllers/users.ts
+++ src/core/controllers/users.ts
@@ -1,56 +1,49 @@
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { Users, IUser } from '../models/users';
 import bcrypt from 'bcrypt';
 import { getAccessToken, handleError } from '../helpers';
 
 export const signup = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const name: string = req.body.name;
-        const email: string = req.body.email;
-        const password: string = req.body.password;
-    
-        if (!name || !email || !password) {
-            return handleError(res, 400, 'All Fields are Mandatory')
-        }
+  try {
+    const name: string = req.body.name;
+    const email: string = req.body.email;
+    const password: string = req.body.password;
 
-        if (await Users.findOne({where: { email }})) {
-            return handleError(res, 400, 'User Already Exists. Login Instead')
-        } else {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const result = { name, email, password: hashedPassword };
-            const user = await Users.create(result);
-            
-            if (user) {
-                res.status(201).send({ token: getAccessToken({name: user.name, email: user.email, id: user.id}) });
-                return;
-            } else {
-                return handleError(res, 400, 'Invalid Data sent during signup process')
-            }
-        }
-    } catch (err) {
-        console.error(err)
-        return handleError(res, 500, `${err}`)
+    if (!name || !email || !password) {
+      return handleError(res, 400, 'All Fields are Mandatory');
     }
-}
+
+    if (await Users.findOne({ where: { email } })) {
+      return handleError(res, 400, 'User Already Exists. Login Instead');
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const result = { name, email, password: hashedPassword };
+      const user = await Users.create(result);
+
+      if (user) {
+        res.status(201).send();
+        return;
+      } else {
+        return handleError(res, 400, 'Invalid Data sent during signup process');
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    return handleError(res, 500, `${err}`);
+  }
+};
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-	const email: string = req.body.email;
-	const password: string = req.body.password;
+  const email: string = req.body.email;
+  const password: string = req.body.password;
 
-	if (!email || !password) {
-        return handleError(res, 400, 'All Fields are Mandatory')
-	}
-	const user: IUser | null = await Users.findOne({ email });    
+  if (!email || !password) {
+    return handleError(res, 400, 'All Fields are Mandatory');
+  }
+  const user: IUser | null = await Users.findOne({ email });
 
-	if (user) {
-		const hashedPassword: string = user.password;
-
-		if (await bcrypt.compare(password, hashedPassword)) {
-            const accessToken = getAccessToken({name: user.name, email: user.email, id: user.id})
-			res.status(200).send({ token: accessToken });
-            return;
-		}
-	} else {
-        return handleError(res, 401, 'Email or Password is invalid')
-	}
+  if (user) {
+  } else {
+    return handleError(res, 401, 'Email or Password is invalid');
+  }
 };
--- src/core/interfaces/models.ts
+++ src/core/interfaces/models.ts
@@ -1,7 +1,12 @@
-import { Types } from 'mongoose';
+export enum SecurityQuestions {
+  MOTHER_MAIDEN_NAME = "What is your mother's maiden name?",
+  NAME_OF_FIRST_PET = 'What is the name of your first pet?',
+  NAME_OF_CITY_YOU_WERE_BORN_IN = 'What is the name of the city where you were born?',
+  NAME_OF_YOUR_PRIMARY_SCHOOL = 'What is the name of your primary school?'
+}
 
 export interface User {
-    name: string;
-    email: string;
-    password: string;
+  username: string;
+  security_q: SecurityQuestions;
+  security_a: string;
 }
