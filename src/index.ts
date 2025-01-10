a096db8ecdd7d09117017f4ed1f5f707d894dcd7
RICHYDOS <richardosunmu@gmail.com>
2025-01-10 21:39:13 +0100

    debugging

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,14 @@ import { router as multiplyRouter } from './scaling/routers/multiply';
 import type { CustomContext } from './scaling/types/CustomContext';
 import type { SessionData } from './scaling/types/SessionData';
 
+function initial(): SessionData {
+  return {
+    route: '',
+    leftOperand: 0,
+    rightOperand: 0
+  };
+}
+
 const app = express();
 const port = settings.port || 5000;
 const bot = new Bot<CustomContext>(settings.botToken);
@@ -37,30 +45,18 @@ app.listen(port, () => {
 
 app.use(errorHandler);
 
+console.log('Part 1');
+
 // 3. Attach a session middleware and specify the initial data
-bot.use(
-  session({
-    initial: (): SessionData => ({
-      route: '',
-      leftOperand: 0,
-      rightOperand: 0
-    }),
-    getSessionKey: (ctx) => {
-      // Use `chat.id` if available, otherwise `from.id`
-      if (ctx.chat) {
-        return `chat:${ctx.chat.id}`;
-      } else if (ctx.from) {
-        return `user:${ctx.from.id}`;
-      }
-      // Return null for updates without `chat` or `from` (skips session handling)
-      return undefined;
-    }
-  })
-);
+bot.use(session({ initial }));
+console.log('Part 2');
 
 // 4. Attach all routers to the bot as middleware
 bot.use(addRouter);
+console.log('Part 3');
 bot.use(multiplyRouter);
+console.log('Part 4');
 
 // 5. Attach all composers to the bot as middleware
 bot.use(composer);
+console.log('Part 5');
